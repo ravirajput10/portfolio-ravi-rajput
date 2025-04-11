b1b4e3933406bff446813b9ac48b08a9cf11f66e
Ravi Rajput <ravi81rajput@gmail.com>
2025-04-11 20:32:12 +0530

    Fixed ci.yml file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Port-folio</h1>
+      <h1 className="text-3xl font-bold">Portfolio Ravi Rajput</h1>
       <Image src="/window.svg" alt="window" width={100} height={100} />
     </div>
   );
